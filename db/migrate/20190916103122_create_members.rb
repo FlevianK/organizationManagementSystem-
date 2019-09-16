d9af8accbbf763a6a3fd7b72e0f9ff958c172936
FlevianK <flevian.kanaiza@andela.com>
2019-09-16 13:44:18 +0300

    Correct datatype

--- db/migrate/20190916103122_create_members.rb
+++ db/migrate/20190916103122_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :members do |t|
       t.string :name
-      t.number :contacts
+      t.integer :contacts
       t.string :residence
       t.date :join_date
       t.boolean :born_again
