3033579d322b718a5767aba6865eceead544a8b3
FlevianK <flevian.kanaiza@andela.com>
2019-10-16 17:30:53 +0300

    Fix debug tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
   let(:user) { build(:user) }
-  let(:headers) { valid_headers.except('Authorization') }
+  let(:headers) { valid_headers }
   let(:valid_attributes) do
     attributes_for(:user, password_confirmation: user.password)
   end
@@ -10,7 +10,7 @@ RSpec.describe 'Users API', type: :request do
   # User signup test suite
   describe 'POST /signup' do
     context 'when valid request' do
-      before { post '/signup', params: valid_attributes.to_json, headers: headers }
+      before { post '/signup', params: valid_attributes, headers: headers }
 
       it 'creates a new user' do
         expect(response).to have_http_status(201)
