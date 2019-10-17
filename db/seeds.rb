966ebaacda86df6140278a6e1a6e47f5c372b8cc
FlevianK <flevian.kanaiza@andela.com>
2019-10-17 16:41:49 +0300

    Add seeders details

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-user = User.create!({name: "", email: "", password: "", password_confirmation: ""})
+user = User.create!({name: ENV['NAME'], email: ENV['EMAIL'], password: ENV['PASSWORD'], password_confirmation: ENV['PASSWORD']})
